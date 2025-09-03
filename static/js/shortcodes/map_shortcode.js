1efe7c76dc7847ec92b1e5032c7b1557c6fbe697
pelfrank <francesco.peluso16@gmail.com>
2025-09-03 22:19:53 +0200

    Fixed map zoom
    Fixed the zoom on the map in order to show the entire bounding box in the map box

--- static/js/shortcodes/map_shortcode.js
+++ static/js/shortcodes/map_shortcode.js
@@ -1,6 +1,6 @@
 let placeCoordinates = [ mapData['coordinates'][1] , mapData['coordinates'][0] ];
 let placeBoundingBox = swapCoordinates();
-let placeMinZoom = 14; //mapData['min_zoom'];
+let placeMinZoom = 14;
 let placeMaxZoom = 999;
 
 let tileLayerUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png' //Da aggiornare in modo dinamico
@@ -11,17 +11,26 @@ let extras = {attribution: "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USD
         //console.log(mapData);
         //console.log(placeCoordinates);
 
-        var map = L.map('map').setView(placeCoordinates, placeMinZoom);
-        $('#map-collapse').on('shown.bs.collapse', function () {
-            map.invalidateSize();
+        $.ajax({
+            url: 'https://api.meteo.uniparthenope.it/places/'+mapData['place_id'],
+            success: function(data){
+                placeMinZoom = data['zoom']['min'];
+                console.log(placeMinZoom);
+                placeMaxZoom = data['zoom']['max'];
+                var map = L.map('map').setView(placeCoordinates, 8);
+                $('#map-collapse').on('shown.bs.collapse', function () {
+                    map.invalidateSize();
+                });
+                L.tileLayer(tileLayerUrl, extras).addTo(map);
+                var polygon = L.polygon(placeBoundingBox).addTo(map);
+            }
         });
-        L.tileLayer(tileLayerUrl, extras).addTo(map);
-        var polygon = L.polygon(placeBoundingBox).addTo(map);
+
+
     });
 })(jQuery);
 
 function swapCoordinates(){
-    //console.log(mapData['bbox']);
 
     let fixedCoordinates = [];
 
